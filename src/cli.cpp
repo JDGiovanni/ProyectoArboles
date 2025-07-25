c1c6acbbcaa6862bb00b2f81efc6be4a727e5b97
Mariangel2811 <mariangelcampos0217@gmail.com>
2025-07-24 22:38:13 -0400

    Create cli.cpp

---
+++ src/cli.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+
+std::map<std::string, std::function<void(std::vector<std::string> args)>> Cli::commands;
+
+/**
+ * \brief Finds a command by name.
+ *
+ * \param name The name of the command to find.
+ *
+ * \return true if the command was found, false otherwise.
+ */
+bool Cli::find(std::string name) {
+    return Cli::commands.find(name) != Cli::commands.end();
+}
+
+/**
+ * \brief Adds a command to the database.
+ *
+ * \param name The name of the command.
+ * \param func The function to call when the command is invoked.
+ *
+ * \return true if the command was successfully added, false otherwise.
+ */
+bool Cli::addCommand(std::string name, std::function<void(std::vector<std::string> args)> func) {
+    Cli::commands[name] = func;
+    return true;
+}
